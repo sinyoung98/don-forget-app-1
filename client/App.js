87245e27975c4fe44fdee2f6be964ea907f77c1b
김호정 <hjnk125@gmail.com>
2020-10-16 10:10:43 +0900

    [ADD]Intro 페이지와 로그인페이지 1차 셋업하였습니다.

--- client/App.js
+++ client/App.js
@@ -2,12 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Intro from './component/Intro';
+import Signin from './component/Signin';
+
+//ver 4.x
+// 전환하게 될 컴포넌트 등록
+// const Stack = createStackNavigator({
+//   "Intro": { screen: Intro },
+//   "Signin": { screen: Signin },
+// }, {
+//   // 맨 처음에 띄울 페이지
+//   intialRouteName: "Intro"
+// })
+
+// version 4.x: createStackNavigator()에 객체로 screen을 설정,
+// key=스크린 이름(변수) / value=컴포넌트 이름
+
+// version 5.x: <Stack.Screen>을 통해 각각의 스크린을 설정
+// name=스크린 이름(변수) / component=컴포넌트 이름
+const Stack = createStackNavigator()
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Intro">
+        <Stack.Screen name="Intro" component={Intro} options={{ title: 'Don-forget' }} />
+        <Stack.Screen name="Signin" component={Signin} options={{ title: 'Login' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
